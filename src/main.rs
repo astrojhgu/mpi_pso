6eb70717273bae4b45a3ebaaaac899cb09d8598a
Gu Junhua <jhgu@nao.cas.cn>
2019-02-18 20:10:09 +0800

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -26,32 +26,35 @@ fn main() {
         &world,
     );
 
-    while !pso.converged(0.7, 1e-11, 1e-11) {
-        pso.sample(&mut rng, 1.193, 1.193, &world);
-        if rank == 0 {
-            if let Some(ref gbest) = pso.gbest {
-                eprintln!("\n{:?} {}", gbest.position, gbest.fitness);
-            } else {
-                eprint!(".")
-            }
 
-            let mut f_last = File::create("last_state.txt").unwrap();
+    for _i in 0..10 {
+        while !pso.converged(0.7, 1e-11, 1e-11) {
+            pso.sample(&mut rng, 1.193, 1.193, &world);
+            if rank == 0 {
+                if let Some(ref gbest) = pso.gbest {
+                    eprintln!("\n{:?} {}", gbest.position, gbest.fitness);
+                } else {
+                    eprint!(".")
+                }
 
-            for (j, p) in pso.swarm.iter().enumerate() {
-                {
-                    let fname = format!("particle_{}.dat", j + 1);
+                let mut f_last = File::create("last_state.txt").unwrap();
 
-                    let mut f = match OpenOptions::new().append(true).open(&fname) {
-                        Ok(f) => f,
-                        _ => File::create(&fname).unwrap(),
-                    };
-                    for x in &p.position.0 {
-                        let _ = write!(&mut f_last, "{}", x);
-                        let _ = write!(&mut f, "{} ", x);
+                for (j, p) in pso.swarm.iter().enumerate() {
+                    {
+                        let fname = format!("particle_{}.dat", j + 1);
+
+                        let mut f = match OpenOptions::new().append(true).open(&fname) {
+                            Ok(f) => f,
+                            _ => File::create(&fname).unwrap(),
+                        };
+                        for x in &p.position.0 {
+                            let _ = write!(&mut f_last, "{}", x);
+                            let _ = write!(&mut f, "{} ", x);
+                        }
+
+                        let _ = writeln!(&mut f, "{}", p.fitness);
+                        let _ = writeln!(&mut f_last);
                     }
-
-                    let _ = writeln!(&mut f, "{}", p.fitness);
-                    let _ = writeln!(&mut f_last);
                 }
             }
         }
